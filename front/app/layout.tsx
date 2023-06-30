90b73068d6e909b05c0429a22e51dce5bff85668
YannBrooklyn <yann.massoro@gmail.com>
2023-06-30 16:55:50 +0200

    save

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -12,9 +12,17 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
-            <body>
-                
+        <html lang="fr">
+            <body style={{backgroundColor:'#2F2F2F'}}>
+                <header className='flex items-center justify-between flex-col gap-4 w-12/12'>
+                    <h2 className='border-solid text-blue-600 border-white'>NAME</h2>
+                    <ul className='flex gap-8'>
+                        <li className='none white'></li>
+                        <li className='none white'>Accueil</li>
+                        <li className='none white'>Connexion</li>
+                    </ul>
+                    <hr className='w-12/12'/>
+                </header>
                 {children}
 
             </body>
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,5 +1,14 @@
-export default function test() {
+export default function Accueil() {
     return (
-        <main>ttt</main>
+        <main className=" flex flex-col items-center w-12/12">
+            <h2 className="text-white" style={{fontSize:"3vh"}}>Actualité</h2>
+            <section className="w-11/12">
+                <div className="flex flex-col items-center" style={{backgroundColor:"#292929", borderColor:"black", borderWidth:"0.2vh", borderRadius:"5%"}}>
+                    <h2>Le titre</h2>
+                    <p className="w-10/12 text-justify text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sollicitudin eros non egestas faucibus. Nunc vitae ultricies elit, vel venenatis arcu. Curabitur suscipit libero a quam mattis, ac faucibus urna varius. Aliquam et sem at arcu condimentum laoreet. Sed eros ipsum, semper id justo sit amet, consectetur mollis massa. Quisque hendrerit, dolor sed ultrices sodales, felis velit rhoncus arcu, at bibendum augue lorem eu elit. Maecenas sollicitudin commodo odio, non mattis velit venenatis quis. Praesent imperdiet quam eu augue lacinia varius. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque tincidunt in nisl et suscipit. In dictum feugiat porta. Nullam quis leo eget elit ornare finibus consectetur aliquet velit. Duis a accumsan felis. Vivamus sit amet egestas libero, eu varius elit.
+Aenean interdum, ipsum eu sodales sagittis, lectus dolor imperdiet nunc, sit amet volutpat nisi dui quis sem. Donec nec vulputate sem. Maecenas maximus vitae erat eget pulvinar. Suspendisse mauris tellus, fringilla et risus viverra, feugiat commodo metus. Donec consequat augue at nisl pretium hendrerit. Phasellus luctus volutpat quam, sed lacinia purus fermentum sed. Quisque non tristique augue. Morbi vestibulum at elit quis mollis.</p>
+                </div>
+            </section>
+        </main>
         )
     } 
